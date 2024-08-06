8ab5db85fefbaa58f9082faf83ab1222a7a0f757
Sher110106 <sher.singh.ug23@plaksha.edu.in>
2024-08-06 14:49:36 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      for deploying to aws
+
+    </div>
+
+
+  )}
+export default App
+
